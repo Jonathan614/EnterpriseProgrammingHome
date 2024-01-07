63246d863260d8591721b59ed684a6693e532fb8
Jonathan614 <79285788+Jonathan614@users.noreply.github.com>
2024-01-07 17:24:10 +0100

    added PassportPhoto file

--- Presentation/Controllers/TicketController.cs
+++ Presentation/Controllers/TicketController.cs
@@ -38,18 +38,48 @@ namespace Presentation.Controllers
         }
 
         [HttpPost]
-        public IActionResult Book(BookTicketViewModel data, [FromServices] IWebHostEnvironment host)
+        public IActionResult Book(BookTicketViewModel data, [FromServices] IWebHostEnvironment host, IFormFile file)
         {
 
-            logger.LogInformation("data.Flight:  " + data.FlightVM);
+            string uniqueFilename = "";
+
+            try
+            {
+
+                if (file != null)
+                {
+                    logger.LogInformation($"Ticket ID {data.Id} has a file", "info");
+                    uniqueFilename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    logger.LogInformation($"Ticket ID {data.Id} now has id: {uniqueFilename}", "info");
+                    string absolutePath = hostService.WebRootPath + @"\Images\" + uniqueFilename;
+                    logger.LogInformation($"Ticket ID {data.Id} and id: {uniqueFilename} will be stored at {absolutePath}", "info");
+                    using (var stream = System.IO.File.Create(absolutePath)) 
+                    {
+                        logger.LogInformation($"Ticket ID {data.Id} and id: {uniqueFilename} is saving the file", "info");
+
+                        file.CopyTo(stream); 
+                    } 
+                    logger.LogInformation($"Ticket ID {data.Id} and id: {uniqueFilename} has file saved successfully", "info");
+
+                    data.passportPhotoPath = "/Images/" + uniqueFilename; 
+
+                    ticketsService.AddTicket(data.row, data.column, data.flightId, data.passport, data.pricePaid, data.cancelled, data.passportPhotoPath);
+
+                }
+                else
+                {
+                    ticketsService.AddTicket(data.row, data.column, data.flightId, data.passport, data.pricePaid, data.cancelled, uniqueFilename);
+                }
                 
-                ticketsService.AddTicket(data.row, data.column, data.flightId, data.passport, data.pricePaid, data.cancelled);
 
-                return RedirectToAction("GetPurchasedTickets", "TicketHist");
-      
+                ViewBag.Message = "Ticket booked successfully";
+            }
+            catch (Exception ex) //innerException
+            {
+                ViewBag.Error = "There was a problem adding a new Ticket. make sure all the fields are correctly filled";
+            }
+            return RedirectToAction("GetPurchasedTickets", "TicketHist");
+
         }
-
-
-
-    }
+    }      
 }
